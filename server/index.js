03ed9d44e42e6809f6acb0d50d3a285aacddf0c3
mat <reallymathew@gmail.com>
2017-04-06 13:33:22 -0500

    install mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 import 'babel-polyfill';
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+
+mongoose.Promise = global.Promise;
 
 const HOST = process.env.HOST;
 const PORT = process.env.PORT || 8080;
@@ -7,19 +11,27 @@ const PORT = process.env.PORT || 8080;
 console.log(`Server running in ${process.env.NODE_ENV} mode`);
 
 const app = express();
+exports.app = app;
 
 app.use(express.static(process.env.CLIENT_PATH));
 
 function runServer() {
     return new Promise((resolve, reject) => {
-        app.listen(PORT, HOST, (err) => {
-            if (err) {
-                console.error(err);
-                reject(err);
-            }
+        let databaseUri = process.env.DATABASE_URI || global.databaseUri || "mongodb://localhost/feels";
+        mongoose
+        .connect(databaseUri)
+        .then(function() {
+            console.log("db connected...")
 
-            const host = HOST || 'localhost';
-            console.log(`Listening on ${host}:${PORT}`);
+            app.listen(PORT, HOST, (err) => {
+                if (err) {
+                    console.error(err);
+                    reject(err);
+                }
+
+                const host = HOST || 'localhost';
+                console.log(`Listening on ${host}:${PORT}`);
+            });
         });
     });
 }
@@ -27,3 +39,5 @@ function runServer() {
 if (require.main === module) {
     runServer();
 }
+
+module.exports = runServer;
